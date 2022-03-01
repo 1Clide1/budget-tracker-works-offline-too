78fe84026258863bbe7818b60f3a184e497b29cb
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-03-01 18:07:44 -0500

    added the service-worker finshed the files to cache, able to install and activate the service worker

---
+++ public/service-worker.js
@@ -0,0 +1,49 @@
+// grab the files to cache
+const FILES_TO_CACHE = [
+  // adding / and index.html just incase that way it's definetly going to be added
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/manifest.json",
+  "/js/index.js",
+  "/idb.js",
+  "/icons/icon-72x72.png",
+  "/assets/images/icons/icon-96x96.png",
+  "/assets/images/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-512x512.png",
+];
+// cache keys
+const CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install the service worker
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("files were pre-cached!!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// activate the service worker and remove any old cached files
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("removed the old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
